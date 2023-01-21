d6fb34f5ea4646b96c7ae76f0fbef853a67a36e7
Andeson Silva <andsilva607@gmail.com>
2023-01-21 17:22:53 -0300

    Extraindo componente GridProducts

---
+++ lib/views/components/product_grid.dart
@@ -0,0 +1,28 @@
+import 'package:app_loja_virtual/controller/controller.dart';
+import 'package:app_loja_virtual/models/product.dart';
+import 'package:app_loja_virtual/views/components/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> loadProducts =
+        Provider.of<Controller>(context, listen: false).returnProducts;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadProducts.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (context, index) {
+        return ProductItem(product: loadProducts[index]);
+      },
+    );
+  }
+}
--- lib/views/products_overview.dart
+++ lib/views/products_overview.dart
@@ -1,40 +1,18 @@
-import 'package:app_loja_virtual/controller/controller.dart';
-import 'package:app_loja_virtual/models/data/products_data.dart';
-import 'package:app_loja_virtual/models/product.dart';
-import 'package:app_loja_virtual/views/components/product_item.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:app_loja_virtual/views/components/product_grid.dart';
 
-class ProductsOverView extends StatefulWidget {
+import 'package:flutter/material.dart';
+
+class ProductsOverView extends StatelessWidget {
   const ProductsOverView({super.key});
 
   @override
-  State<ProductsOverView> createState() => _ProductsOverViewState();
-}
-
-class _ProductsOverViewState extends State<ProductsOverView> {
-  @override
   Widget build(BuildContext context) {
-    final List<Product> loadProducts =
-        Provider.of<Controller>(context, listen: false).returnProducts;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Minha Loja"),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (context, index) {
-          return ProductItem(product: loadProducts[index]);
-        },
-      ),
+      body: const ProductGrid(),
     );
   }
 }
