50b91c5d8e211949d6ced222d44cc7b6207bb2e9
Andeson Silva <andsilva607@gmail.com>
2023-01-24 21:44:50 -0300

    Editando pagina de detalhe de produtos

--- lib/views/product_detail.dart
+++ lib/views/product_detail.dart
@@ -15,6 +15,43 @@ class ProductDetail extends StatelessWidget {
         title: Text(product.getTitle),
         centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "R\$ ${product.getPrice}",
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Text(
+                product.getDescription,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
