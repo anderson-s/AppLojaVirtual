86f245e2e7316857b8ae3c053141af58673e9748
Andeson Silva <andsilva607@gmail.com>
2023-01-25 18:16:45 -0300

    Controlando o foco dos componentes de texto, além de trabalhar com multilines

--- lib/views/product_page_form.dart
+++ lib/views/product_page_form.dart
@@ -8,12 +8,55 @@ class ProductPageForm extends StatefulWidget {
 }
 
 class _ProductPageFormState extends State<ProductPageForm> {
+  final priceFocus = FocusNode();
+  final descriptionFocus = FocusNode();
+  @override
+  void dispose() {
+    priceFocus.dispose();
+    descriptionFocus.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Formulário de Produto"),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: "Nome"),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(priceFocus);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "Preço"),
+                textInputAction: TextInputAction.next,
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                focusNode: priceFocus,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(descriptionFocus);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "Descrição"),
+                textInputAction: TextInputAction.next,
+                focusNode: descriptionFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
