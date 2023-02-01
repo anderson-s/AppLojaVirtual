69ad04dc27e14453c5d9b418c79260933c9e053e
Andeson Silva <andsilva607@gmail.com>
2023-02-01 17:37:47 -0300

    Trabalhando com slivers

--- lib/views/product_detail.dart
+++ lib/views/product_detail.dart
@@ -11,49 +11,60 @@ class ProductDetail extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.getTitle),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: false,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                product.getTitle,
+                textAlign: TextAlign.center,
+              ),
+              background: Hero(
                 tag: product.getId,
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
+                child: AspectRatio(
+                  aspectRatio: 3.4,
+                  child: Image.network(
+                    product.imageUrl,
+                    // fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
-            const SizedBox(
-              height: 10,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "R\$ ${product.getPrice}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  child: Text(
+                    product.getDescription,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              
+              ],
             ),
-            Text(
-              "R\$ ${product.getPrice}",
-              style: const TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10,
-              ),
-              child: Text(
-                product.getDescription,
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
